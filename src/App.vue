2852ea31cf545c81cf382edac35ce3f5a7104154
dieromeo <diegoromeo99@icloud.com>
2023-12-12 17:48:54 +0100

    feat: add footer

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <script>
+import FooterComponent from './components/FooterComponent.vue';
 import HeaderComponent from './components/HeaderComponent.vue';
 import MainComponent from './components/MainComponent.vue';
 export default {
     name: 'App',
     components: {
         HeaderComponent,
-        MainComponent
+        MainComponent,
+        FooterComponent
     }
 }
 </script>
@@ -13,6 +15,7 @@ export default {
 <template>
     <HeaderComponent />
     <MainComponent />
+    <FooterComponent />
 </template>
 
 <style lang="scss">
---
+++ src/components/FooterComponent.vue
@@ -0,0 +1,31 @@
+<script>
+import Logo from './Logo.vue';
+
+export default {
+    name: 'FooterComponent',
+    components: {
+        Logo
+    }
+}
+</script>
+
+<template>
+    <footer>
+        <Logo />
+    </footer>
+</template>
+
+<style lang="scss">
+@use '../style/partials/variables' as *;
+
+footer {
+    background-color: $founded-bg;
+    padding: 10px;
+    text-align: center;
+
+    img[data-v-42795242] {
+        height: 40px;
+        margin: 0;
+    }
+}
+</style>
